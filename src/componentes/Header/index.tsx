ae878592f5b65c22b00120786548eefdf65d8f05
Pedro Feld <pedro.gael.feld@gmail.com>
2025-11-08 16:49:20 -0300

    feat: adicionando renderização condicional simulando login

--- src/componentes/Header/index.tsx
+++ src/componentes/Header/index.tsx
@@ -1,7 +1,17 @@
 function Header() {
+    const usuarioLogado = true;
+    let message;
+
+    if (usuarioLogado) {
+        message = <p>Bem-vindo de volta!</p>;
+    } else {
+        message = <p>Por favor, faça login.</p>;
+    }
+
     return (
         <header>
             <h1>Meu blog</h1>
+            {message}
         </header>
     )
 }
