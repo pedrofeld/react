00bf6996ad6dfaf29191c8ae4e57a184c71ece67
Pedro Feld <pedro.gael.feld@gmail.com>
2025-11-08 15:44:34 -0300

    feat: criando o rascunho de um projeto de um blog

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,33 @@ export function App() {
   const hello = "Hello, React!"
   return (
     <> 
-      <h1>{hello}</h1>
-      <h2>Estamos aprendendo React.</h2>
+      <header>
+        <h1>Meu blog</h1>
+      </header>
+
+      <main>
+        <h3>{hello}</h3>
+        <p>Estamos aprendendo React criando um blog de receitas.</p>
+
+        <article>
+          <h3>Receitas</h3>
+          <img src="https://picsum.photos/200/200" alt="Imagem aleatória"/>
+          <p>Bloco de chocolate</p>
+
+          <div>
+            <img src="https://github.com/pedrofeld.png" alt="Foto de perfil do autor"/>
+            <div>
+              <span>Pedro Feld</span>
+              <span>08/11/2025</span>
+            </div>
+            <p>Delicioso bolo de chocolate</p>
+          </div>
+        </article>
+      </main>
+
+      <footer>
+        <p>₢ 2025 Meu blog</p>
+      </footer>
     </>
   )
 }
@@ -12,5 +37,6 @@ export function App() {
   Considerações:
   - <> fragment vazio, substitui uma div desnecessária (que seria um nó a mais no DOM e pioraria a performance ou afetaria a estilização)
   - Elementos como variáveis e comentários precisam estar entre chaves {// comentário}
+  - O link https://picsum.photos/ fornece imagens aleatórias para testes, o "/200/300" define a largura e altura da imagem em pixels
   -
 */
