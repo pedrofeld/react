a3b1712b4455246dc50584167314e9b11b5f3611
Pedro Feld <pedro.gael.feld@gmail.com>
2025-11-08 16:51:29 -0300

    feat: adicionando renderização condicional com '&&'

--- src/componentes/Footer/index.tsx
+++ src/componentes/Footer/index.tsx
@@ -1,7 +1,10 @@
 function Footer() {
+    const message = true;
+
     return (
         <footer>
             <p>₢ 2025 Meu blog</p>
+            {message && <p>Obrigado por visitar meu blog!</p>}
         </footer>
     )
 }
