b1e81881dabab82ed774440f4b28e8060231a286
Pedro Feld <pedro.gael.feld@gmail.com>
2025-11-12 18:39:34 -0300

    style: estilização da lista de posts

--- src/componentes/PostsList/index.tsx
+++ src/componentes/PostsList/index.tsx
@@ -1,23 +1,30 @@
 import { posts } from '../../posts'
 import { IPost } from '../../types'
 import Post from '../Post'
+import { Wrapper, Container, Title } from './styles'
 
 export default function PostsList({ posts }: { posts: IPost[] }) {
   return (
-    <>
-      {posts.map((post) => (
-        <Post
-          key={post.id}
-          id={post.id}
-          titulo={post.titulo}
-          imagem={post.imagem}
-          ingredientes={post.ingredientes}
-          instrucoes={post.instrucoes}
-          autor={post.autor}
-          criadoEm={post.criadoEm}
-          descricao={post.descricao}
-        />
-      ))}
-    </>
+    <Wrapper>
+      <Container>
+        <Title>
+          <h1>Posts recentes</h1>
+          <span>Escolhemos a dedo nossos artigos favoritos</span>
+        </Title>
+        {posts.map((post) => (
+          <Post
+            key={post.id}
+            id={post.id}
+            titulo={post.titulo}
+            imagem={post.imagem}
+            ingredientes={post.ingredientes}
+            instrucoes={post.instrucoes}
+            autor={post.autor}
+            criadoEm={post.criadoEm}
+            descricao={post.descricao}
+          />
+        ))}
+      </Container>
+    </Wrapper>
   )
 }
