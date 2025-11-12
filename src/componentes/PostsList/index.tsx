9500730226d7cf7ac4b566b67e14bf6c5cdb6f1c
Pedro Feld <pedro.gael.feld@gmail.com>
2025-11-12 18:45:49 -0300

    style: melhorando a exibição dos posts

--- src/componentes/PostsList/index.tsx
+++ src/componentes/PostsList/index.tsx
@@ -1,7 +1,7 @@
 import { posts } from '../../posts'
 import { IPost } from '../../types'
 import Post from '../Post'
-import { Wrapper, Container, Title } from './styles'
+import { Wrapper, Container, Title, Posts } from './styles'
 
 export default function PostsList({ posts }: { posts: IPost[] }) {
   return (
@@ -11,19 +11,21 @@ export default function PostsList({ posts }: { posts: IPost[] }) {
           <h1>Posts recentes</h1>
           <span>Escolhemos a dedo nossos artigos favoritos</span>
         </Title>
-        {posts.map((post) => (
-          <Post
-            key={post.id}
-            id={post.id}
-            titulo={post.titulo}
-            imagem={post.imagem}
-            ingredientes={post.ingredientes}
-            instrucoes={post.instrucoes}
-            autor={post.autor}
-            criadoEm={post.criadoEm}
-            descricao={post.descricao}
-          />
-        ))}
+        <Posts>
+            {posts.map((post) => (
+              <Post
+                key={post.id}
+                id={post.id}
+                titulo={post.titulo}
+                imagem={post.imagem}
+                ingredientes={post.ingredientes}
+                instrucoes={post.instrucoes}
+                autor={post.autor}
+                criadoEm={post.criadoEm}
+                descricao={post.descricao}
+              />
+            ))}
+        </Posts>
       </Container>
     </Wrapper>
   )
